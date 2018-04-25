a7d8563b492a9300b6cc0aa3ad3063da2b8f6dec
johadi10 <jim.hadi10@gmail.com>
2018-04-26 00:21:25 +0100

    add the necessary icons for the project

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import { AppRegistry, YellowBox } from 'react-native';
+import App from './src/App';
 
+console.ignoredYellowBox = ['Remote debugger'];
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 AppRegistry.registerComponent('notebook', () => App);
