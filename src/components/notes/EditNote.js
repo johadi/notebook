6ea6ff8ec9a3aff6f2a323f9bbe3ea1cda8f9c85
johadi10 <jim.hadi10@gmail.com>
2018-05-09 22:11:06 +0100

    feat(note): implement the add note feature

--- src/components/notes/EditNote.js
+++ src/components/notes/EditNote.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import { updateNote, clearUpdateNoteErrors, clearUpdatedNote } from '../../actions';
 import { CheckIcon, CreateNote } from '../../common'
 import { Alert } from "react-native";
 
-export class EditNote extends Component {
-  state = {
-    selectedValue: 'Uncategorised',
-    titleText: '',
-    bodyText: ''
+class EditNoteContainer extends Component {
+  static propTypes = {
+    navigation: PropTypes.object.isRequired,
+    noteState: PropTypes.object.isRequired,
+    clearUpdateNoteErrors: PropTypes.func.isRequired,
+    clearUpdatedNote: PropTypes.func.isRequired,
+    updateNote: PropTypes.func.isRequired,
   };
+
+  state = {
+    note: {
+      title: '',
+      body: '',
+      category: 'Uncategorised'
+    },
+    noteId: null
+  };
+
 
   static navigationOptions = ({navigation}) => {
     const { handleEditNote } = navigation.state.params || {};
@@ -22,37 +38,99 @@ export class EditNote extends Component {
     this.props.navigation.setParams({ handleEditNote: this.handleEditNote });
 
     const { params } = this.props.navigation.state;
-    const { note: {category, title, body} } = params ? params : {};
+    const { note: {id, category, title, body} } = params ? params : {};
+    const note = { category, title, body };
 
-    this.setState({
-      selectedValue: category,
-      titleText: title,
-      bodyText: body
-    });
-
+    this.setState({ note, noteId: id });
   }
 
-  handlePickerValueChange = (itemValue, itemIndex) => {
-    this.setState({selectedValue: itemValue})
+  componentDidUpdate() {
+    const { updateNoteValidationError, updateNoteFailure, updatedNote } = this.props.noteState;
+
+    if (updateNoteValidationError) {
+      Alert.alert(
+        'Note update failed',
+        'Your note must have a title and a body',
+        [
+          { text: 'OK', onPress: this.props.clearUpdateNoteErrors }
+        ]
+      );
+
+      return;
+    }
+
+    if (updateNoteFailure) {
+      Alert.alert(
+        'Note Update failed',
+        updateNoteFailure,
+        [
+          { text: 'OK', onPress: this.props.clearUpdateNoteErrors }
+        ]
+      );
+
+      return;
+    }
+
+    if(updatedNote) {
+      this.props.navigation.state.params.resetViewNoteScreenParam(updatedNote);
+      this.props.navigation.goBack();
+    }
+  }
+
+  componentWillUnmount() {
+    this.props.clearUpdatedNote();
+    this.props.clearUpdateNoteErrors();
+  }
+
+  /**
+   * Handles the changes in the select picker. This function sets the note category values
+   * @param itemValue - the picker value
+   */
+  handlePickerValueChange = (itemValue) => {
+    const {note} = this.state;
+    note['category'] = itemValue;
+    this.setState({ note });
   };
 
-  handleTextInputChange = (text) => {
-    this.setState({text});
+  /**
+   * Handles the input text changes
+   * @param inputName - the name of the input
+   * @param inputValue - the value of the input as it changes
+   */
+  handleTextInputChange = (inputName, inputValue) => {
+    const { note } = this.state;
+    note[inputName] = inputValue;
+    this.setState({note});
   };
 
+  /**
+   * Triggers the action that handles the update note
+   */
   handleEditNote = () => {
-    Alert.alert('Edited');
+    const { note, noteId } = this.state;
+    this.props.updateNote(note, noteId);
   };
 
-  render(){
+  render() {
+    const { title, body, category } = this.state.note;
     return (
       <CreateNote
         handlePickerValueChange={this.handlePickerValueChange}
-        handleTextInputChange = {this.handleTextInputChange}
-        titleText={this.state.titleText}
-        bodyText={this.state.bodyText}
-        pickerSelectedValue={this.state.selectedValue}
+        handleTitleInputChange = {value => this.handleTextInputChange('title', value)}
+        handleBodyInputChange = {value => this.handleTextInputChange('body', value)}
+        titleText={title}
+        bodyText={body}
+        pickerSelectedValue={category}
       />
     )
   }
 }
+
+const mapStateToProps = ({noteState}) => {
+  return { noteState }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ updateNote, clearUpdateNoteErrors, clearUpdatedNote }, dispatch);
+};
+export const EditNote = connect(mapStateToProps, mapDispatchToProps)(EditNoteContainer);
